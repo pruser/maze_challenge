50504e7ec5ee1982ffd688aac53cf95f59bc01ee
pruser <pruser@users.noreply.github.com>
2017-01-16 00:09:39 +0100

    basic building blocks to create cpp compile time maze solver

---
+++ maze.cpp
@@ -0,0 +1,25 @@
+enum  {
+	Free = 0,
+  Wall = 1,
+  Start = 2,
+  Finish = 3
+};
+
+template <typename... T>
+struct List {};
+
+template <int n>
+struct I {
+	static const int val = n;
+};
+
+using M = List<
+ 	List< I<0>, I<0>, I<0>, I<0> >,
+	List< I<0>, I<0>, I<0>, I<0> >,
+ 	List< I<0>, I<0>, I<0>, I<0> >
+>;
+
+
+int main() {
+	return 0;	
+}
