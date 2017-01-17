4d2e4b84a5a41b76ac93c2d4cbe28f7c2a5089fc
pruser <pruser@users.noreply.github.com>
2017-01-17 23:00:24 +0100

    TypedList and get functions for List and TypedList added

--- maze.cpp
+++ maze.cpp
@@ -12,6 +12,9 @@ enum {
 template <typename... T>
 struct List {};
 
+template <typename T, T... Args>
+struct TypedList {};
+
 template <int n>
 struct I {
   static const int val = n;
@@ -66,6 +69,26 @@ constexpr auto find_position2(X x, List<T...> t) {
   return find_position2(0, x, t);
 }
 
+constexpr auto get(std::size_t i, List<>) {
+	return std::optional<int>{};
+}
+
+template <typename T1, typename... T>
+constexpr auto get(std::size_t i, List<T1, T...>) {
+	if(i==0) return std::make_optional(T1::val);
+  	else return get(i-1, List<T...>{});
+}
+
+template <typename T>
+constexpr auto get(std::size_t i, TypedList<T>) {
+    return std::optional<T>{};
+}
+
+template <typename T, T t1, T... args>
+constexpr auto get(std::size_t i, TypedList<T, t1, args...>) {
+    if(i==0) return std::make_optional(t1);
+    else return get(i-1, TypedList<T, args...>{});
+}
 
 int main() {
   {
@@ -78,6 +101,13 @@ int main() {
       List< I<0>, I<0>, I<0>, I<0> >
     >;
     static_assert(find_position2(I<1>{}, M1{}) == std::make_optional(Position(1,2)), "");
+  }
+  {
+    static_assert(get(2, List<I<0>, I<2>, I<1>, I<0>>{}) == std::optional<int>{1}, "");
+  }
+  
+  {
+    static_assert(get(2, TypedList<int, 0, 2, 1, 0>{}) == std::optional<int>{1}, "");
   }
   
   return 0;
