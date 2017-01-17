fc12fbf2fd5a4c4756a0ab617879eec9627c3534
pruser <pruser@users.noreply.github.com>
2017-01-17 22:10:15 +0100

    size_t i passed as function argument to find_position functions

--- maze.cpp
+++ maze.cpp
@@ -23,47 +23,47 @@ using M = List<
   List< I<0>, I<0>, I<0>, I<0> >
 >;
 
-template <size_t i, typename X>
-constexpr auto find_position(X x, List<>){
+template <typename X>
+constexpr auto find_position(size_t i, X x, List<>){
   return std::optional<size_t>{};
 }
 
-template <size_t i, typename X, typename... T>
-constexpr auto find_position(X x, List<X, T...>){
+template <typename X, typename... T>
+constexpr auto find_position(std::size_t i, X x, List<X, T...>){
   return std::optional<size_t>{i};
 }
 
-template <size_t i, typename X, typename T1, typename... T>
-constexpr auto find_position(X x, List<T1, T...>){
-  return find_position<i+1, X>(x, List<T...>{});
+template <typename X, typename T1, typename... T>
+constexpr auto find_position(size_t i, X x, List<T1, T...>){
+  return find_position(i+1, x, List<T...>{});
 }
 
 template <typename X, typename T>
 constexpr auto find_position(X x, T t) {
-  return find_position<0>(x, t);
+  return find_position(0, x, t);
 }
 
 using Position = std::pair<std::size_t, std::size_t>;
 
-template <size_t i, typename X>
-constexpr auto find_position2(X x, List<>){
+template <typename X>
+constexpr auto find_position2(size_t i, X x, List<>){
   return std::optional<Position>{};
 }
 
-template <size_t i, typename X, typename... T1, typename... T>
-constexpr auto find_position2(X x, List<List<T1...>, T...>){
+template <typename X, typename... T1, typename... T>
+constexpr auto find_position2(size_t i, X x, List<List<T1...>, T...>){
   auto pos = find_position(x, List<T1...>{}); 
   if(pos) {
     return std::make_optional(Position{i, pos.value()});
   }
   else {
-    return find_position2<i+1, X>(x, List<T...>{});
+    return find_position2(i+1, x, List<T...>{});
   }
 }
 
 template <typename X, typename... T>
 constexpr auto find_position2(X x, List<T...> t) {
-  return find_position2<0>(x, t);
+  return find_position2(0, x, t);
 }
 
 
